186997f165babcf0f1ad01bb8f67e9e013237ffe
aapiane09 <aapiane09@gmail.com>
2017-01-13 15:43:16 -0800

    Migrated index.html and styles.css, created server.js, runs nodemon successfully

---
+++ server.js
@@ -0,0 +1,40 @@
+// SERVER-SIDE JAVASCRIPT
+
+//require express in our app
+var express = require('express');
+// generate a new express app and call it 'app'
+var app = express();
+// serve static files from public folder
+app.use(express.static(__dirname + '/public'));
+
+/**********
+ * ROUTES *
+ **********/
+
+/*
+ * HTML Endpoints
+ */
+
+app.get('/', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/templates/:name', function templates(req, res) {
+  var name = req.params.name;
+  res.sendFile(__dirname + '/views/templates/' + name + '.html');
+});
+
+// ALL OTHER ROUTES (ANGULAR HANDLES)
+// redirect all other paths to index
+app.get('*', function homepage (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+/**********
+ * SERVER *
+ **********/
+
+// listen on port 3000
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Express server is running on http://localhost:3000/');
+});
