22ea6a486ab60c435af2510b5119c55d99706d2e
aapiane09 <aapiane09@gmail.com>
2017-01-16 13:24:44 -0800

    Added color scheme

--- public/scripts/controllers/TrackResultsController.js
+++ public/scripts/controllers/TrackResultsController.js
@@ -5,14 +5,21 @@ angular
     .module('Songparsr')
     .controller('TrackResultsController', TrackResultsController);
 
-TrackResultsController.$inject = ['$http'];
+TrackResultsController.$inject = ['$routeParams', '$location', '$http'];
 
-function TrackResultsController ($http) {
-  console.log('TrackResultsController Function Sanity Check!');
-  var vm = this;
-  vm.exampleTrack = {
-    trackName: 'Formation',
-    artistName: 'Beyoncé',
-    albumName: 'Lemonade'
-  } ;
-}
+// function TrackResultsController ($routeParams, $location, $http) {
+//   console.log('TrackResultsController Function Sanity Check!');
+//   var vm = this;
+//   var query;
+//   var type;
+//   $http({
+//     method: 'GET',
+//     url: 'https://api.spotify.com/v1/search?q=' + query + '&type=' + type
+//   }).then(function successCallback(response) {
+//     console.log(response.data)
+//   }, function errorCallback(response) {
+//     console.log('There was an error getting the data', response);
+//   });
+//
+//
+// }
